044d44b302e8714b4e41d9dda2c5633b6c4cf8b3
warrenjfeng <warrenfeng@alumni.unc.edu>
2025-11-21 16:48:16 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for GitHub Pages compatibility
+  base: '/where-were-we/', // Use relative paths for GitHub Pages compatibility
 })
 
 
